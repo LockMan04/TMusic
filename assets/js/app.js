3b538e34d3b28bc2a2b88fb37fb307c8751f12d4
La Thanh Toan <2200007030@nttu.edu.vn>
2023-08-12 00:50:03 +0700

    update something

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,25 +7,7 @@ var typedEffect = new Typed(".multiText", {
     backDelay : 10000
 })
 
-// Play Music
-window.onload = function() {
-  var audioPlayer = document.getElementById('audio-player');
-  
-function playMusic() {
-  if (audioPlayer.paused) {
-    audioPlayer.play();
-    document.querySelector('.bi-play').classList.add('bi-pause');
-    document.querySelector('.bi-play').classList.remove('bi-play');
-  } else {
-    audioPlayer.pause();
-    document.querySelector('.bi-pause').classList.add('bi-play');
-    document.querySelectorAlluerySelector('.bi-pause').classList.remove('bi-pause');
-  }
-}
-};
-
 // Time Music
-    var audioPlayer = document.getElementById('audio-player');
     var audioPlayer = document.getElementById('audio-player');
     var musicRange = document.getElementById('music-range');
     var musicDuration = document.getElementById('music-duration');
@@ -73,25 +55,6 @@ function padZero(number) {
   return number.toString().padStart(2, '0');
 }
 
-// Trình Phát Nhạc
-// Nút Play Music
-function playMusic() {
-    if (audioPlayer.paused) {
-    audioPlayer.play();
-    document.querySelector('.bi-play').classList.add('bi-pause');
-    document.querySelector('.bi-play').classList.remove('bi-play');
-    } else {
-    audioPlayer.pause();
-    document.querySelector('.bi-pause').classList.add('bi-play');
-    document.querySelector('.bi-pause').classList.remove('bi-pause');
-    }
-}
-// Nút Next, Back
-function nextSong() {
-  
-}
-
-
 // Đóng mở Home / Tìm Kiếm
 const homeButton = document.getElementById('home');
 const searchButton = document.getElementById('search');
@@ -111,3 +74,192 @@ homeButton.addEventListener('click', function() {
   searchButton.classList.remove('text-underline');
   homeButton.classList.add('text-underline');
 })
+
+
+// Tester
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const imagePlay = $('.song-playing-img img')
+const nameSong = $('.song-playing-title-namesong')
+const nameSinger = $('.name-singer')
+const audio = $('#audio-player')
+const playBtn = $('#play-pause')
+const timeRange = $('#music-range')
+const timeStart = $('#music-duration')
+const timeEnd = $('#music-duration-end')
+
+
+window.onload = function() { 
+  timeRange.value = 0
+  timeStart.textContent = '00:00'
+}
+
+const app = {
+  isPlaying: false,
+  currentIndex: 10,
+  songs: [
+  {
+    id: '1',
+    name: 'nếu lúc đó',
+    type: 'Bài hát',
+    singer: 'tlinh, 2pillz',
+    path: './assets/music/songs/NeuLucDo-tlinh2pillz-8783613.mp3',
+    image: './assets/music/img-songs/neulucdo.jpg'
+  },
+  {
+    id: '2',
+    name: 'Anh Nhớ Ra',
+    type: 'Bài hát',
+    singer: 'Vũ ft.Trang',
+    path: './assets/music/songs/Anh Nhớ Ra (feat. TRANG) - Vũ..mp3',
+    image: './assets/music/img-songs/Anh Nhớ Ra (feat. TRANG) - Vũ..jpg'
+  },
+  {
+    id: '3',
+    name: 'Anh Đã Ổn Hơn',
+    type: 'Bài hát',
+    singer: 'MKC',
+    path: './assets/music/songs/AnhDaOnHon-MCK-8804113.mp3',
+    image: './assets/music/img-songs/anh da on hon.jpg'
+  },
+  {
+    id: '4',
+    name: 'Chìm Sâu',
+    type: 'Bài hát',
+    singer: 'RPT MKC',
+    path: './assets/music/songs/Chìm Sâu - RPT MCK.mp3',
+    image: './assets/music/img-songs/Chìm Sâu - RPT MCK.jpg'
+  },
+  {
+    id: '5',
+    name: 'Chuyện Đôi Ta',
+    type: 'Bài hát',
+    singer: 'Emcee L(Da LAB), ft.Muộii',
+    path: './assets/music/songs/Chuyện Đôi Ta (feat. Muộii) - Emcee L (Da LAB).mp3',
+    image: './assets/music/img-songs/Chuyện Đôi Ta (feat. Muộii) - Emcee L (Da LAB).jpg'
+  },
+  {
+    id: '6',
+    name: 'Ghệ Đẹp',
+    type: 'Bài hát',
+    singer: 'Cain',
+    path: './assets/music/songs/Ghệ Đẹp - Cain.mp3',
+    image: './assets/music/img-songs/Ghệ Đẹp - Cain.jpg'
+  },
+  {
+    id: '7',
+    name: 'ghệ iu dấu của em ơi',
+    type: 'Bài hát',
+    singer: 'tlinh',
+    path: './assets/music/songs/ghệ iu dấu của em ơi - tlinh.mp3',
+    image: './assets/music/img-songs/ghệ iu dấu của em ơi - tlinh.jpg'
+  },
+  {
+    id: '8',
+    name: 'Sinh Ra Đã Là Thứ Đối Lập Nhau',
+    type: 'Bài hát',
+    singer: 'Emcee L (Da LAB), ft.Badbies',
+    path: './assets/music/songs/Sinh Ra Đã Là Thứ Đối Lập Nhau (feat. Badbies) - Emcee L (Da LAB).mp3',
+    image: './assets/music/img-songs/Sinh Ra Đã Là Thứ Đối Lập Nhau (feat. Badbies) - Emcee L (Da LAB).jpg'
+  },
+  {
+    id: '9',
+    name: 'Trước Khi Em Tồn Tại',
+    type: 'Bài hát',
+    singer: 'Thắng',
+    path: './assets/music/songs/Trước Khi Em Tồn Tại - Thắng.mp3',
+    image: './assets/music/img-songs/Trước Khi Em Tồn Tại - Thắng.jpg'
+  },
+  {
+    id: '10',
+    name: 'Waitting For You',
+    type: 'Bài hát',
+    singer: 'MONO',
+    path: './assets/music/songs/Waiting For You - MONO.mp3',
+    image: './assets/music/img-songs/Waiting For You - MONO.jpg'
+  },
+  {
+    id: '11',
+    name: 'She Said',
+    type: 'Bài hát',
+    singer: 'WEAN, NAOMI',
+    path: './assets/music/songs/She Said - WEAN, NAOMI - NhacHay360.mp3',
+    image: './assets/music/img-songs/she said.jpg'
+  }
+  ],
+  
+  render: function () {
+    const htmls = this.songs.map(song => {
+      return `
+            <div class="library-playlist-item" style="--i:${song.id}">
+              <div class="library-playlist-img">
+                  <img src="${song.image}" alt="">
+              </div>
+              <div class="library-playlist-title">
+                  <p>${song.name}</p>
+                  <p>${song.type}<i class="bi bi-dot"></i>${song.singer}</p>
+              </div>
+            </div>
+      `
+    })
+    $('.library-playlist').innerHTML = htmls.join('')
+  },
+  defineProperties: function() {
+    Object.defineProperty(this, 'currentSong', {
+      get: function() {
+        return this.songs[this.currentIndex]
+      }
+    })
+  },
+
+  loadCurrentSong: function () {
+    const _this = this
+    imagePlay.src = this.currentSong.image
+    nameSinger.textContent = this.currentSong.singer
+    nameSong.textContent = this.currentSong.name
+    audio.src = this.currentSong.path
+
+    playBtn.onclick = function () {
+      if(_this.isPlaying) {
+        audio.pause()
+      } else {
+        audio.play()
+      }
+    }
+
+    // Sử lý play
+    audio.onplay = function () { 
+      _this.isPlaying = true
+      playBtn.classList.replace('bi-play', 'bi-pause')
+    }
+
+    // Sử lý pause
+    audio.onpause = function () {
+      _this.isPlaying = false
+      playBtn.classList.replace('bi-pause', 'bi-play')
+    }
+
+    audio.ontimeupdate = function () { 
+      const timeNow = audio.currentTime
+      const timeDuration = audio.duration
+
+      timeRange.value = timeNow / timeDuration * 100
+
+    }
+    
+  },
+
+  // Web bắt đầu
+  start: function () {
+    // Lấy ra bài hát đầu tiên
+    this.defineProperties()
+
+    // Load bài hát 
+    this.loadCurrentSong()
+
+    // Load playlist
+    this.render();
+  }
+}
+
+app.start();
